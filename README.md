7f672bf04dadeb6a7fe2d2990f696bd573976945
panxiaoan <xiaoan.pan@qq.com>
2020-08-04 13:53:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Docker Common Images
---
+++ mysql/mysql-8.0.21-33060/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# mkdir /Users/panxiaoan/work/docker/mysql-8.0.21-33060/data
+# mkdir /Users/panxiaoan/work/docker/mysql-8.0.21-33060/conf
+
+docker-compose -f stack.yml up
