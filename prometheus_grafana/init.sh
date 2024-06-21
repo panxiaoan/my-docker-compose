dddf07562e7eaf33613ef2b78cfcc70d387751be
panxiaoan <xiaoan.pan@qq.com>
2024-06-21 13:18:11 +0800

    fix: 新增 253 node_exporter

--- prometheus_grafana/init.sh
+++ prometheus_grafana/init.sh
@@ -2,6 +2,10 @@ mkdir -p ./volumes/prometheus/data
 mkdir -p ./volumes/grafana/data
 mkdir -p ./volumes/grafana/provisioning
 
+sudo chmod -R 777 ./volumes/prometheus/data
+sudo chmod -R 777 ./volumes/grafana/data
+sudo chmod -R 777 ./volumes/grafana/provisioning
+
 firewall-cmd --zone=public --add-port=9090/tcp --permanent
 firewall-cmd --zone=public --add-port=3000/tcp --permanent
 firewall-cmd --zone=public --add-port=9100/tcp --permanent
